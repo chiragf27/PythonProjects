9d578e2155a1862540974f375867002dc66b285d
Chirag Ferwani <chiragferwani@gmail.com>
2023-07-17 15:57:01 +0530

    add file

---
+++ BMICalculator/main.py
@@ -0,0 +1,20 @@
+height = input(“Enter height in cm”)
+weight = input(“Enter weight in Kg”)
+int(weight)
+float(height)
+bmi = round(weight/height**2)
+if bmi <18.5:
+print(f"Your bmi is {bmi}, you
+are Underweight")
+elif bmi <= 25:
+print(f"Your bmi is {bmi}, you
+are Normal weight")
+elif bmi <= 30:
+print(f"Your bmi is {bmi}, you
+are Overweight")
+elif bmi <= 35:
+print(f"Your bmi is {bmi}, you
+are obese")
+else:
+print(f"Your bmi is {bmi}, you
+are clinically obese")
